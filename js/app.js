c34e628db59ce13fa56a4e1fcc91450b2c342e78
neotrixstdr <josemanuel.delahorra@gmail.com>
2020-06-01 18:47:28 +0200

    Mod Rutas del SW

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 
+var url = window.location.href;
+var swLocation = '/twitor/sw.js';
+
+
 if( navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+
+    if( url.includes('localhost')){
+        swLocation='/sw.js';
+    }
+
+    navigator.serviceWorker.register( swLocation );
 }
 
 
